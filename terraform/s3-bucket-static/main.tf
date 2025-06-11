2bd2b10517db43ed6bcb06aaf10a0294b819dea3
LuizFernandin <luizx2012x>
2025-06-11 15:16:27 -0300

    feat: enable tags

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
-    bucket = "static-site-${var.bucket_name}
+    bucket = "static-site-${var.bucket_name}"
 
     website {
         index_document = "index.html"
         error_document = "404.html"
     }
 
-    @REM tags = {
-    @REM     Name = "Static Site Bucket"
-    @REM     Environment = "Production"
-    @REM }
+    tags = {
+        Name = "Static Site Bucket"
+        Environment = "Production"
+    }
 }
