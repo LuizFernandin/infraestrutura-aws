72cbab73b086ee37ad1706aed10b2bbac91b4cc5
LuizFernandin <luizx2012x>
2025-06-12 17:45:04 -0300

    feat: add infra s3-start-stepfunction

--- terraform/s3-start-stepfunction/s3.tf
+++ terraform/s3-start-stepfunction/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.bucket.id
 
-  eventbridge {
-    events = ["s3:ObjectCreated:*"]
-  }
+@REM   eventbridge {
+@REM     events = ["s3:ObjectCreated:*"]
+@REM   }
 }
